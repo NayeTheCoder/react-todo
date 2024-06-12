b9151184d96c73cc5a710d6aa1a5ec539636249d
Nayeli <nayeli585052@gmail.com>
2024-06-11 19:20:47 -0500

    week 1 list

--- weekly assignments/src/App.js
+++ weekly assignments/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+var todoList = [
+  {
+  id: 1,
+  title:"Complete assignment"
+  },
+  {
+    id:2,
+    title:"Go to the gym"
+  },
+  {
+    id:3,
+    title:"pick up nephew"
+  }
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+        return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
