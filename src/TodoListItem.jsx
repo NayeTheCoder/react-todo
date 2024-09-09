117246d36788b2cbf1f4a77185256666cc2dae0b
Nayeli <nayeli585052@gmail.com>
2024-09-09 07:53:11 -0500

    Week 12 css

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import style from"./TodoListItem.module.css";
 
 function TodoListItem ({title, id, onRemoveTodo}) {
   return (
-  <li>
+  <li className={style.ListItem}>
     {title}
-    <button type="button" onClick={() =>  onRemoveTodo(id)}>Remove</button>
+    <button type="button" onClick={() =>  onRemoveTodo(id)} className={style.Button}>X</button>
     </li>
   );
 }
